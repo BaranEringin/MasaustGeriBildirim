8dbec0c795c467c6aa1b418540d39088a595ec1e
Yusuf Baran Eringin <152855200+BaranEringin@users.noreply.github.com>
2024-01-30 12:27:14 +0300

    main.py revision

---
+++ main.py
@@ -0,0 +1,8 @@
+from plyer import notification
+notification.notify(
+     title = 'Massage',
+     message = 'How are you?',
+     app_icon = None,
+     app_name = 'Notifications',
+     timeout = 10,
+     )
